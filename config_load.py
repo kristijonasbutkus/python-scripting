fa376a961ec7af4b7fe46dfe587b649c55960e5f
Kristijonas Butkus <kristijonas.butkus@gmail.com>
2022-01-13 06:44:40 -0800

    add configUtils for working with configuration json file

---
+++ config_load.py
@@ -0,0 +1,53 @@
+from collections import namedtuple
+import json
+from tokenize import String
+from types import SimpleNamespace
+from typing import List
+
+class ConfigUtils:
+
+    __config = None
+
+    def __init__(self, config = "config.json"):
+        self.__loadConfig(config)
+        if not self.__loadConfig:
+            exit("Unable to load json configuration")
+
+    def __loadConfig(self, config):
+        try:
+            with open(config, "r", encoding="utf-8") as json_file:
+                return json.load(json_file)
+        except:
+            print('Could not load json config')  
+
+    def loadJsonConfig(config = "config.json"):
+        try:
+            with open(config, "r", encoding="utf-8") as jsonFile:
+                return json.load(jsonFile)
+                
+        except:
+            print('Could not load json config')  
+
+    def getDevicesFromConfig(dict):
+        try:
+            deviceList = []
+            Device = namedtuple('Device', 'model connection_type commands')
+            for x in range(len(dict['device'])):
+                deviceList.append(Device(model=dict['device'][x]['model'],
+                    connection_type=dict['device'][x]['connection_type'],
+                    commands=dict['device'][x]['commands']
+                ))
+            return deviceList
+        except Exception as e:
+            print(e)
+
+    def getDevice(userRequestedDeviceName, deviceList):
+        devices = []
+        try:
+            for x in range(len(deviceList)):
+                if userRequestedDeviceName == deviceList[x].model:
+                    return deviceList[x]
+            return None
+        except Exception as e:
+            print(e)
+
